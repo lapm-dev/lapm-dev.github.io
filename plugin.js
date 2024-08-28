d3d5ba9ffd7c48ae41a97c17fc8bb117706c4e83
lapm-dev <lampa@stably.cloud>
2024-08-28 13:48:38 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -6,10 +6,12 @@
         icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21.5 12C21.5 17.2467 17.2467 21.5 12 21.5C6.75329 21.5 2.5 17.2467 2.5 12C2.5 6.75329 6.75329 2.5 12 2.5C17.2467 2.5 21.5 6.75329 21.5 12Z" stroke="currentColor" stroke-width="2"/><path d="M12 7V12L15 15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
 
         init: function() {
-            setTimeout(function() {
-                ShikimoriPlugin.addSettings();
-                ShikimoriPlugin.addMenuButton();
-            }, 100);
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type == 'ready') {
+                    ShikimoriPlugin.addSettings();
+                    ShikimoriPlugin.addMenuButton();
+                }
+            });
         },
 
         addSettings: function() {
@@ -70,14 +72,17 @@
         },
 
         addMenuButton: function() {
-            Lampa.Menu.append({
-                id: 'shikimori',
-                title: 'Shikimori',
-                icon: ShikimoriPlugin.icon,
-                onClick: function() {
+            var settingsMenu = Lampa.Settings.main().render();
+            
+            if (settingsMenu) {
+                var shikimoriButton = $('<div class="settings-folder selector" data-component="shikimori"><div class="settings-folder__icon">' + ShikimoriPlugin.icon + '</div><div class="settings-folder__name">Shikimori</div></div>');
+                
+                shikimoriButton.on('hover:enter', function(){
                     Lampa.Settings.create('shikimori');
-                }
-            });
+                });
+                
+                settingsMenu.find('[data-component="more"]').before(shikimoriButton);
+            }
         },
 
         authorize: function() {
@@ -148,10 +153,6 @@
                     Lampa.Noty.show(Lampa.Lang.translate('shikimori_auth_error'));
                 }
             });
-        },
-
-        start: function() {
-            // Запуск плагина
         }
     };
 
